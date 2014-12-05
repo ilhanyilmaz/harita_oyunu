f583bdd90c1609af4f0860a9d7178de825b4a2b4
ilhan yilmaz <ilhannn@gmail.com>
2014-12-05 19:05:53 +0200

    Changes to be committed: 	new file:   harita_oyun.py 	new file:   il_sinirlari.png

---
+++ harita_oyun.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from random import randint
+
+image = cv2.imread("./il_sinirlari.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+r,threshold = cv2.threshold(gray,220,255,cv2.THRESH_BINARY_INV)
+cv2.imshow('threshold',threshold)
+contours, hierarcy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+i=0
+for contour in contours:
+    width, height = gray.shape
+    x,y,w,h = cv2.boundingRect(contour)
+    mask = np.zeros((width,height),np.uint8)
+
+    cv2.drawContours(image,contours,i,(randint(255),randint(255),randint(255)),1)
+    cv2.drawContours(mask, [contour], 0, 255, -1)
+    city = mask[y:y+h,x:x+w]
+    cv2.imshow('city', city)
+    if cv2.waitKey(10) != -1:
+        break
+    i+=1
+
+cv2.imshow('sinirlar', image)
+
+if cv2.waitKey(0) != -1:
+    cv2.destroyAllWindows()
